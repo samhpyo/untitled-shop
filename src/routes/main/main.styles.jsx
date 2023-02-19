ca560127be44ec7343d607123ba16f9dbdf61847
samhpyo <samhpyo@gmail.com>
2023-02-19 02:11:48 -0800

    Minor fix

--- src/routes/main/main.styles.jsx
+++ src/routes/main/main.styles.jsx
@@ -8,8 +8,8 @@ export const MainContainer = styled.div`
 
 export const Video = styled.video`
   object-fit: cover;
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
   position: fixed;
   top: 0;
   left: 0;
